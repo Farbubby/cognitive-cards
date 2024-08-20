351d9b2c0c45b1add50fc1c0cd82f65f556343d6
farbubby <farhan.mahbub@ucf.edu>
2024-08-19 21:11:25 -0400

    Fixed an edge case with adding a topic with a space

--- src/app/cards/[topic]/page.tsx
+++ src/app/cards/[topic]/page.tsx
@@ -3,7 +3,12 @@ import FlashCardsList from "./flashcard-list";
 import Link from "next/link";
 import LinkButton from "@/components/link-button";
 
+const parseTopic = (topic: string) => {
+  return topic.replaceAll("%20", " ");
+};
+
 export default function Home({ params }: { params: { topic: string } }) {
+  const topic = parseTopic(params.topic);
   return (
     <>
       <div className="flex flex-col gap-8">
@@ -11,8 +16,8 @@ export default function Home({ params }: { params: { topic: string } }) {
           <LinkButton href="/" text={"Back to home"} />
           <LinkButton href="/cards" text="Back to topics" />
         </div>
-        <div className="text-4xl font-bold text-center">{params.topic}</div>
-        <FlashCardsList topic={params.topic} />
+        <div className="text-4xl font-bold text-center">{topic}</div>
+        <FlashCardsList topic={topic} />
       </div>
     </>
   );
